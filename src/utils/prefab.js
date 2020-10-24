2c90dde36aad300c0909dec8feae1538ed960468
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2020-10-24 03:30:19 +0200

    added comments

--- src/utils/prefab.js
+++ src/utils/prefab.js
@@ -13,3 +13,38 @@ module.exports = {
 
     }
 }
+
+/*
+
+Attributes:
+    > name [String, needed]
+        - name of the command
+
+    > aliases [Array of Strings, optional]
+        - aliases for the command
+
+    > usage: [String, optional]
+        - how to use the command (will be displayed in the help message)
+
+    > examples [String, optional]
+        - examples of how to use the command (will be displayed in the help message)
+
+    > cooldown [Number (Seconds), optional]
+        - cooldown of the command
+
+    > perms [Array of Strings, optional]
+        - the perms the user needs to use this command
+
+    > arguments [Array of Objects, optional]
+        - Argument Object:
+            {
+                type: ArgumentType,
+                prompt: String,
+                optional: boolean
+            }
+
+        - ArgumentType: NUMBER, CHANNEL, ROLE, MEMBER_OR_USER           [needed]
+        - prompt: The message to sent if the command user didn't provide            [optional, will not send a message and simply return]
+        - optional: If true, then it will skip it if it wasn't provided by the command user and move to the next argument           [optional, default will be false]
+
+*/
