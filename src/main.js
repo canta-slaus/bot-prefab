66cc059affa553eaf03f64f993c6997f12947d6b
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2020-10-30 00:16:33 +0100

    fixes due to discord api changes

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ const discord = require("discord.js");
 const config = require("../config/config.json");
 const { registerCommands, registerEvents } = require('./utils/registry');
 
-const client = new discord.Client();
+const client = new discord.Client({ws: { intents: discord.Intents.ALL }});
 
 (async () => {
     await client.login(config.TOKEN);
+    console.log(`Logged in as ${client.user.tag}`)
     client.commands = new discord.Collection();
     await registerEvents(client, '../eventHandlers');
     await registerCommands(client, '../commands');
