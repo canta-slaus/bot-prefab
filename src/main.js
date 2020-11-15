bdfcfbc4b8f1b2ce9dbb488b205855abd325e3f9
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2020-11-15 19:28:32 +0100

    connect to database and schema

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 const discord = require("discord.js");
+const mongoose = require('mongoose')
 const config = require("../config/config.json");
 const { registerCommands, registerEvents } = require('./utils/registry');
 
-const client = new discord.Client({ws: { intents: discord.Intents.ALL }});
+const client = new discord.Client({ws: { intents: discord.Intents.ALL }, presence: {status: 'invisible'}});
 
 (async () => {
     await client.login(config.TOKEN);
     console.log(`Logged in as ${client.user.tag}`)
     client.commands = new discord.Collection();
+    client.guildInfoCache = new discord.Collection();
+    client.DBGuild = require('../schemas/guildSchema');
     await registerEvents(client, '../eventHandlers');
     await registerCommands(client, '../commands');
+    await mongoose.connect(config.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    });
 })();
