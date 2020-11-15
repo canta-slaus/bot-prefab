810f2c17a23955ff24bddc90a02d67288c938fce
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2020-11-15 19:28:11 +0100

    added prefix caching, command disabling and try/catch when executing command

--- src/eventHandlers/message.js
+++ src/eventHandlers/message.js
@@ -1,4 +1,3 @@
-const PREFIX = require("../../config/config.json").PREFIX;
 const { processArguments } = require("../utils/utils")
 const { Collection } = require("discord.js")
 const cooldowns = new Collection();
@@ -7,6 +6,18 @@ module.exports = async (client, message) => {
     
     if (message.author.bot) return;
     if (message.channel.type === 'dm') return;
+
+    let guildInfo = client.guildInfoCache.get(message.guild.id)
+    if (!guildInfo) {
+        const fetch = await client.DBGuild.findByIdAndUpdate(message.guild.id, {}, {new: true, upsert: true, setDefaultsOnInsert: true});
+        guildInfo = {};
+        guildInfo['prefix'] = fetch.prefix;
+        guildInfo['disabledCommands'] = fetch.disabledCommands;
+        client.guildInfoCache.set(message.guild.id, guildInfo)
+        console.log(guildInfo)
+    }
+    const PREFIX = guildInfo.prefix
+
     if (!message.content.startsWith(PREFIX)) return;
 
     let msgargs = message.content.substring(PREFIX.length).split(new RegExp(/\s+/));
@@ -15,9 +26,10 @@ module.exports = async (client, message) => {
     const command = await client.commands.get(cmdName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
 
     if (!command) return;
-    if (command.perms) {
-        if (!message.member.hasPermission(command.perms)) return
-    }
+
+    if (guildInfo.disabledCommands.includes(command.name)) return;
+
+    if (command.perms && !message.member.hasPermission(command.perms)) return;
     
     const cd = command.cooldown;
     if (cd) {
@@ -39,5 +51,10 @@ module.exports = async (client, message) => {
 
     if (command.arguments && command.arguments.length !== 0) msgargs = processArguments(message, command.arguments, msgargs)
     if (!msgargs) return;
-    command.execute(client, message, msgargs);
+    try {
+        command.execute(client, message, msgargs);
+    } catch (error) {
+        message.channel.send(`Oops, something went wrong!`)
+        console.log(`Error occured!\nAt command: ${command.name}\nError message: ${error.message}\nError trace: ${error.trace}`)
+    }
 };
