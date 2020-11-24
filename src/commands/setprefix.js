91df359164dbfeff3fc3a64fbe9a392130f0ac94
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2020-11-24 23:18:55 +0100

    serverOwnerOnly: true

--- src/commands/setprefix.js
+++ src/commands/setprefix.js
@@ -5,6 +5,7 @@ module.exports = {
     aliases: ["sp"],
     description: "Set a new prefix for your server.",
     usage: `\`${PREFIX}setprefix [newprefix]\``,
+    serverOwnerOnly: true,
     arguments: [
         {
             type: 'STRING',
@@ -13,7 +14,7 @@ module.exports = {
     ],
     
     execute: async function(client, message, args) {
-        await client.DBGuild.findByIdAndUpdate(message.guild.id, {$set: {prefix: args[0]}}, {new: true, upsert: true, setDefaultsOnInsert: true});
+        await client.DBGuild.findByIdAndUpdate(message.guild.id, {$set: { prefix: args[0]} }, { new: true, upsert: true, setDefaultsOnInsert: true });
         const guildInfo = client.guildInfoCache.get(message.guild.id)
         guildInfo.prefix = args[0]
         client.guildInfoCache.set(message.guild.id, guildInfo)
