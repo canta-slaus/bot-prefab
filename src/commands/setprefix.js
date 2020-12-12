6b5cd787304637a733b14b8186042de1d2b342f5
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2020-12-12 23:42:14 +0100

    add/fix: use regex to make sure the prefix is not messy, added check if the old prefix is the same

--- src/commands/setprefix.js
+++ src/commands/setprefix.js
@@ -1,5 +1,7 @@
 const PREFIX = require('../../config/config.json').PREFIX;
 
+const prefixRegExp = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{1,15}$/;
+
 module.exports = {
     name: "setprefix",
     aliases: ["sp"],
@@ -14,10 +16,12 @@ module.exports = {
     ],
     
     execute: async function(client, message, args) {
-        await client.DBGuild.findByIdAndUpdate(message.guild.id, {$set: { prefix: args[0]} }, { new: true, upsert: true, setDefaultsOnInsert: true });
+        if (!prefixRegExp.test(args[0])) return message.channel.send(`${message.author.username}, that prefix doesn't follow the rules. Please try again.`)
         const guildInfo = client.guildInfoCache.get(message.guild.id)
+        if (guildInfo.prefix === args[0]) return message.channel.send(`${message.author.username}, please make sure to enter a new prefix.`)
+        await client.DBGuild.findByIdAndUpdate(message.guild.id, {$set: { prefix: args[0]} }, { new: true, upsert: true, setDefaultsOnInsert: true });
         guildInfo.prefix = args[0]
         client.guildInfoCache.set(message.guild.id, guildInfo)
-        message.channel.send(`The new prefix is: \`${args[0]}\``)
+        message.channel.send(`${message.author.username}, the new prefix is: \`${args[0]}\``)
     }
 }
