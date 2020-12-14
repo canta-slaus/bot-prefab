b62f08483c5baf7063b8fa8f6cbaa8be3a2dd3e3
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2020-12-14 18:17:26 +0100

    update: added new logs and changed structure a bit

--- src/eventHandlers/message.js
+++ src/eventHandlers/message.js
@@ -5,70 +5,69 @@ const { devs, someServers } = require('../../config/config.json')
 const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
 module.exports = async (client, message) => {
-    if (message.author.bot || message.channel.type === 'dm' || client.blacklistCache.has(message.author.id)) return;
-
-    let guildInfo = client.guildInfoCache.get(message.guild.id)
-    if (!guildInfo) {
-        const fetch = await client.DBGuild.findByIdAndUpdate(message.guild.id, {}, { new: true, upsert: true, setDefaultsOnInsert: true });
-        guildInfo = {};
-        guildInfo['prefix'] = fetch.prefix;
-        if (fetch.disabledCommands) guildInfo['disabledCommands'] = fetch.disabledCommands;
-        if (fetch.commandPerms) guildInfo['commandPerms'] = fetch.commandPerms
-        client.guildInfoCache.set(message.guild.id, guildInfo)
-    }
-    
-    const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(guildInfo.prefix)})\\s*`);
-    if (!prefixRegex.test(message.content)) return;
-
-    const [, matchedPrefix] = message.content.match(prefixRegex);
-    let msgargs = message.content.slice(matchedPrefix.length).trim().split(/ +/);
-    let cmdName = msgargs.shift().toLowerCase();
-    
-    if (message.mentions.has(client.user) && !cmdName) return message.channel.send(`My prefix is \`${guildInfo.prefix}\` or ${client.user}\nTo view a list of my commands, type either \`${guildInfo.prefix}help\` or \`@${client.user.tag} help\``)
-    
-    const command = await client.commands.get(cmdName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
-
-    if (!command) return;
-
-    if (command.devOnly && !devs.includes(message.author.id)) return;
-    if (command.someServersOnly && !someServers.includes(message.guild.id)) return;
-    if (command.serverOwnerOnly && message.guild.ownerID !== message.author.id) return;
-
-    if (guildInfo.disabledCommands.includes(command.name)) return;
-    if (guildInfo.commandPerms && guildInfo.commandPerms[command.name] && !message.member.hasPermission(guildInfo.commandPerms[command.name])) {
-        return message.channel.send(`${message.author.username}, you are missing the following permissions: ${missingPermissions(message.member, guildInfo.commandPerms[command.name])}`)
-    } else if (command.perms && !message.member.hasPermission(command.perms)) {
-        return message.channel.send(`${message.author.username}, you are missing the following permissions: ${missingPermissions(message.member, command.perms)}`)
-    }
-    
-    if (command.clientPerms && !message.guild.me.permissions.has(command.clientPerms)) {
-        return message.channel.send(`${message.author.username}, I am missing the following permissions: ${missingPermissions(message.guild.me, command.clientPerms)}`)
-    }
-
-    const cd = command.cooldown;
-    if (cd) {
-        if (!cooldowns.has(command.name)) {
-            cooldowns.set(command.name, new Collection());
-        }
-
-        const now = Date.now();
-        const timestamps = cooldowns.get(command.name);
-        const cooldownAmount = cd * 1000;
-        if (timestamps.has(message.author.id)) {
-            const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-            if (now < expirationTime) return await message.channel.send(`${message.author.username}, please wait \`${msToTime(expirationTime - now)}\` before using this command again.`)
-        }
-
-        timestamps.set(message.author.id, now);
-        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-    }
-
-    if (command.arguments && command.arguments.length !== 0) msgargs = processArguments(message, command.arguments, msgargs)
-    if (!msgargs) return;
     try {
+        if (message.author.bot || message.channel.type === 'dm' || client.blacklistCache.has(message.author.id)) return;
+
+        let guildInfo = client.guildInfoCache.get(message.guild.id)
+        if (!guildInfo) {
+            const fetch = await client.DBGuild.findByIdAndUpdate(message.guild.id, {}, { new: true, upsert: true, setDefaultsOnInsert: true });
+            guildInfo = {};
+            guildInfo['prefix'] = fetch.prefix;
+            if (fetch.disabledCommands) guildInfo['disabledCommands'] = fetch.disabledCommands;
+            if (fetch.commandPerms) guildInfo['commandPerms'] = fetch.commandPerms
+            client.guildInfoCache.set(message.guild.id, guildInfo)
+        }
+        
+        const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(guildInfo.prefix)})\\s*`);
+        if (!prefixRegex.test(message.content)) return;
+
+        const [, matchedPrefix] = message.content.match(prefixRegex);
+        let msgargs = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+        let cmdName = msgargs.shift().toLowerCase();
+        
+        if (message.mentions.has(client.user) && !cmdName) return message.channel.send(`My prefix is \`${guildInfo.prefix}\` or ${client.user}\nTo view a list of my commands, type either \`${guildInfo.prefix}help\` or \`@${client.user.tag} help\``)
+        
+        const command = await client.commands.get(cmdName)
+
+        if (!command) return;
+
+        if (command.devOnly && !devs.includes(message.author.id)) return;
+        if (command.someServersOnly && !someServers.includes(message.guild.id)) return;
+        if (command.serverOwnerOnly && message.guild.ownerID !== message.author.id) return;
+
+        if (guildInfo.disabledCommands.includes(command.name)) return;
+        if (guildInfo.commandPerms && guildInfo.commandPerms[command.name] && !message.member.hasPermission(guildInfo.commandPerms[command.name])) {
+            return message.channel.send(`${message.author.username}, you are missing the following permissions: ${missingPermissions(message.member, guildInfo.commandPerms[command.name])}`)
+        } else if (command.perms && !message.member.hasPermission(command.perms)) {
+            return message.channel.send(`${message.author.username}, you are missing the following permissions: ${missingPermissions(message.member, command.perms)}`)
+        }
+        
+        if (command.clientPerms && !message.guild.me.permissions.has(command.clientPerms)) {
+            return message.channel.send(`${message.author.username}, I am missing the following permissions: ${missingPermissions(message.guild.me, command.clientPerms)}`)
+        }
+
+        const cd = command.cooldown;
+        if (cd) {
+            if (!cooldowns.has(command.name)) {
+                cooldowns.set(command.name, new Collection());
+            }
+
+            const now = Date.now();
+            const timestamps = cooldowns.get(command.name);
+            const cooldownAmount = cd * 1000;
+            if (timestamps.has(message.author.id)) {
+                const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+                if (now < expirationTime) return await message.channel.send(`${message.author.username}, please wait \`${msToTime(expirationTime - now)}\` before using this command again.`)
+            }
+
+            timestamps.set(message.author.id, now);
+            setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+        }
+
+        if (command.arguments && command.arguments.length !== 0) msgargs = processArguments(message, msgargs, command.arguments)
+        if (msgargs.invalid) return message.channel.send(msgargs.prompt);
         command.execute(client, message, msgargs);
-    } catch (error) {
-        message.channel.send(`Oops, something went wrong!`)
-        console.log(`Error occured!\nAt command: ${command.name}\nError message: ${error.message}\nError trace: ${error.trace}`)
+    } catch (e) {
+        log("ERROR", "src/eventHandlers/message.js", e.message)
     }
 };
--- src/main.js
+++ src/main.js
@@ -2,24 +2,44 @@ const discord = require("discord.js");
 const mongoose = require('mongoose')
 const config = require("../config/config.json");
 const { registerCommands, registerEvents } = require('./utils/registry');
+const { log } = require('./utils/utils')
 
-const client = new discord.Client({ws: { intents: discord.Intents.ALL }});
+const client = new discord.Client({ ws: { intents: discord.Intents.ALL } });
 
 (async () => {
-    await client.login(config.TOKEN);
-    console.log(`Logged in as ${client.user.tag}`)
     client.commands = new discord.Collection();
+    client.categories = new discord.Collection();
     client.guildInfoCache = new discord.Collection();
+
     client.DBGuild = require('../schemas/guildSchema');
-    client.DBConfig = require('../schemas/config')
+    client.DBConfig = require('../schemas/config');
+
     await registerEvents(client, '../eventHandlers');
     await registerCommands(client, '../commands');
-    await mongoose.connect(config.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    });
-    const blacklistFetch = await client.DBConfig.findByIdAndUpdate('blacklist', {}, {new: true, upsert: true, setDefaultsOnInsert: true})
-    client.blacklistCache = new Set(blacklistFetch.blacklisted)
-    console.log('Added all commands, events, schemas and connected to MongoDB.')
+
+    try {
+        await client.login(config.TOKEN);
+        log("SUCCESS", "src/main.js", `Logged in as ${client.user.tag}`);
+    } catch (e) {
+        log("ERROR", "src/main.js", `Error logging in: ${e.message}`);
+    };
+
+    try {
+        await mongoose.connect(config.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+
+        const blacklistFetch = await client.DBConfig.findByIdAndUpdate('blacklist', { }, { new: true, upsert: true, setDefaultsOnInsert: true });
+        client.blacklistCache = new Set(blacklistFetch.blacklisted);
+        
+        log("SUCCESS", "src/main.js", "Connected to the database.")
+    } catch (e) {
+        log("ERROR", "src/main.js", `Error connecting to the database: ${e.message}`)
+        log("ERROR", "src/main.js", "As of now, the prefab heavily relies on a successful connection.\nThere is a short guide on how to setup a MongoDB cluster (online cluster, not localhost) over at https://github.com/canta-slaus/bot-prefab/wiki/Setting-up-a-cluster")
+        process.exit(1)
+    };
+
+    log("SUCCESS", "src/main.js", "Added all commands, categories, events, schemas and connected to MongoDB.");
 })();
