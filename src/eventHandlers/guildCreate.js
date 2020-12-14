a524649b85ae6dbb33bd8598475b2bcef9d5e213
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2020-12-14 18:16:27 +0100

    fix: couple issues regarding the found channels, see update-notes

--- src/eventHandlers/guildCreate.js
+++ src/eventHandlers/guildCreate.js
@@ -1,26 +1,32 @@
 const PREFIX = require('../../config/config.json').PREFIX;
 
 const getDefaultChannel = (guild) => {
-  // get "original" default channel
-  if(guild.channels.cache.has(guild.id))
-    return guild.channels.cache.get(guild.id)
+    let channel;
+    // Get "original" default channel
+    if (guild.channels.cache.has(guild.id)) {
+        channel = guild.channels.cache.get(guild.id)
+        if (channel.permissionsFor(guild.client.user).has("SEND_MESSAGES")) {
+            return guild.channels.cache.get(guild.id)
+        }
+    }
 
-  // Check for a "general" channel, which is often default chat
-  const generalChannel = guild.channels.cache.find(channel => channel.name === "general");
-  if (generalChannel)
-    return generalChannel;
-  // Now we get into the heavy stuff: first channel in order where the bot can speak
-  return guild.channels
-   .filter(c => c.type === "text" &&
-     c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
-   .sort((a, b) => a.position - b.position ||
-     Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
-   .first();
+    // Check for a "general" channel, which is often default chat
+    channel = guild.channels.cache.find(channel => channel.name === "general" && channel.permissionsFor(guild.client.user).has("SEND_MESSAGES"));
+    if (channel) return channel;
+
+    // Now we get into the heavy stuff: first channel in order where the bot can speak
+    return guild.channels.cache
+        .filter(c => c.type === "text" &&
+                     c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
+        .sort((a, b) => a.position - b.position ||
+                        Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
+        .first();
 }
 
 module.exports = async (client, guild) => {
-  if (guild.available) {
-    const channel = getDefaultChannel(guild);
-    channel.send(`Thanks for adding me! My prefix is \`${PREFIX}\`\nFor a list of commands, type \`${PREFIX}help\``)
-  }
+    if (guild.available) {
+        const channel = getDefaultChannel(guild);
+        if (!channel) return;
+        channel.send(`Thanks for adding me! My prefix is \`${PREFIX}\`\nFor a list of commands, type \`${PREFIX}help\``)
+    }
 }
