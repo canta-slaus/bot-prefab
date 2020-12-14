3aa3ecf792c9919dbff7b622368f666f4fa4551c
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2020-12-14 18:15:51 +0100

    updated: added category feature

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,41 +1,63 @@
-const PREFIX = require('../../config/config.json').PREFIX;
 const EMBED_COLOR = require('../../config/config.json').EMBED_COLOR;
 const { MessageEmbed } = require("discord.js");
 
+const replacePrefix = (string, guildPrefix) => {
+    return string.replace(/PREFIX/g, guildPrefix);
+};
+
 module.exports = {
-    name: 'help',
+    name: "help",
+    category: "Misc",
     aliases: ["h"],
     description: "Get help on commands.",
-    usage: `To get help on a specific command, use \`${PREFIX}help [command name]\` (without the [ ]).\nFor a full list of all commands, simply use \`${PREFIX}help\`.`,
-    examples: `\`${PREFIX}help ping\``,
+    usage: "To get help on a specific command, use \`PREFIXhelp [command name]\` (without the [ ]).\nFor a full list of all commands, simply use \`PREFIXhelp\`.",
+    examples: "\`PREFIXhelp ping\`",
     
     execute: async function(client, message, args) {
+        let guildPrefix = client.guildInfoCache.get(message.guild.id).prefix;
         if (!args.length) {
-            let hEmbed = new MessageEmbed()
-            .setTitle("Command Categories")
-            .setColor(EMBED_COLOR)
-            .setDescription(`Use \`${PREFIX}help [command name]\` to get more info on a specific command, for example: \`${PREFIX}help ping\``)
-            .addField("Misc", "`help`, `ping`")
-	        .setTimestamp()
-            .setThumbnail(client.user.displayAvatarURL())
-            await message.channel.send(hEmbed);
+            return defaultHelp(client, message, guildPrefix);
         }
-        else {
-            const cmdname = args[0].toLowerCase();
-            const command = client.commands.get(cmdname) || client.commands.find(c => c.aliases && c.aliases.includes(cmdname));
 
-            if (!command) return message.channel.send(`${message.author.username}, that\'s not a valid command!`)
+        const queryName = args.join(' ').toLowerCase()
+        const command = client.commands.get(queryName) || client.commands.find(c => c.aliases && c.aliases.includes(queryName));
+        const category = client.categories.get(queryName)
 
+        if (command) {
+            
             let hEmbed = new MessageEmbed()
             .setTitle(`${command.name}`)
-            .setDescription(`${command.description}`)
+            .setAuthor(command.category ? command.category : 'No category')
             .setColor(EMBED_COLOR)
             .setTimestamp()
-            if (command.usage) hEmbed.addField("Usage", `${command.usage}`)
-            if (command.aliases && command.aliases.length !== 0) hEmbed.addField("Aliases", `${command.aliases.join(', ')}`)
-            if (command.examples) hEmbed.addField("Examples", `${command.examples}`)
+            
+            if (command.description) hEmbed.setDescription(replacePrefix(command.description, guildPrefix))
+            if (command.usage) hEmbed.addField("Usage", replacePrefix(command.usage, guildPrefix))
+            if (command.aliases && command.aliases.length !== 0) hEmbed.addField("Aliases", '`' + command.aliases.join('`, `') + '`')
+            if (command.examples) hEmbed.addField("Examples", replacePrefix(command.examples, guildPrefix))
             if (client.guildInfoCache.get(message.guild.id).disabledCommands.includes(command.name)) hEmbed.setAuthor('This command is currently disabled in this server.')
+            
             message.channel.send(hEmbed);
-        }
+        } else if (category) {
+            let hEmbed = new MessageEmbed()
+            .setTitle(category[0])
+            .setColor(EMBED_COLOR)
+            .setTimestamp()
+            .setDescription('`' + category.slice(1).join('`, `') + '`')
+
+            message.channel.send(hEmbed)
+        } else defaultHelp(client, message, guildPrefix)
     }
+}
+
+function defaultHelp(client, message, guildPrefix) {
+    let hEmbed = new MessageEmbed()
+    .setTitle("Command Categories")
+    .setColor(EMBED_COLOR)
+    .setDescription(`Use \`${guildPrefix}help [category]\` to get more info on a category, for example: \`${guildPrefix}help misc\``)
+    .setTimestamp()
+    .setThumbnail(client.user.displayAvatarURL())
+    .addField('Categories', client.categories.map(c => '> ' + c[0]).join('\n'))
+
+    message.channel.send(hEmbed);
 }
--- src/utils/registry.js
+++ src/utils/registry.js
@@ -1,5 +1,6 @@
 const fs = require('fs').promises;
 const path = require('path');
+const { log } = require('./utils')
 
 async function registerCommands(client, dir) {
     let files = await fs.readdir(path.join(__dirname, dir));
@@ -13,13 +14,36 @@ async function registerCommands(client, dir) {
             if(file.endsWith(".js")) {
                 try {
                     let cmdModule = require(path.join(__dirname, dir, file));
-                    let { aliases } = cmdModule;
-                    client.commands.set(cmdModule.name, cmdModule);
+                    let { name, aliases, category } = cmdModule;
+
+                    if (!name) {
+                        log("WARNING", "src/registry.js", `The command '${path.join(__dirname, dir, file)}' doesn't have a name`)
+                        continue;
+                    }
+                    if (!cmdModule.execute) {
+                        log("WARNING", "src/registry.js", `The command '${name}' doesn't have an execute function`)
+                        continue;
+                    }
+
+                    client.commands.set(name, cmdModule);
+
+                    if (category) {
+                        let commands = client.categories.get(category.toLoweCase())
+                        if (!commands) commands = [category]
+                        commands.push(name)
+                        client.categories.set(category.toLowerCase(), name)
+                    } else {
+                        log("WARNING", "src/registry.js", `The command '${name}' doesn't have a category, it will default to 'No category'.`)
+                        let commands = client.categories.get('no category')
+                        if (!commands) commands = ['No category']
+                        commands.push(name)
+                        client.categories.set('no category', commands)
+                    }
+
                     if(aliases && aliases.length !== 0)
                         aliases.forEach(alias => client.commands.set(alias, cmdModule));
-                }
-                catch(err) {
-                    console.log(err);
+                } catch (e) {
+                    log("ERROR", "src/registry.js", `Error loading commands: ${e.message}`);
                 }
             }
         }
@@ -40,9 +64,8 @@ async function registerEvents(client, dir) {
                 try {
                     let eventModule = require(path.join(__dirname, dir, file));
                     client.on(eventName, eventModule.bind(null, client));
-                }
-                catch(err) {
-                    console.log(err);
+                } catch(e) {
+                    log("ERROR", "src/registry.js", `Error loading events: ${e.message}`)
                 }
             }
         }
