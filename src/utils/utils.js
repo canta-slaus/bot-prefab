b5fa43b88cd9772f29b14082ab7458eddf84f824
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2020-12-17 00:46:40 +0100

    added a few checks to paginate()

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -156,19 +156,27 @@ async function whitelist(client, userID) {
  * @example Examples can be seen in `src/utils/utils.md`
  */
 async function paginate(message, embeds, options) {
-    const pageMsg = await message.channel.send({ embed: embeds[0] })
-    await pageMsg.react(reactions[0])
-    await pageMsg.react(reactions[1])
-    await pageMsg.react(reactions[2])
+    const pageMsg = await message.channel.send({ embed: embeds[0] });
+
+    let stop = false;
+
+    for (const emote of reactions) {
+        if (stop) return;
+        await pageMsg.react(emote).catch(e => stop = true);
+    }
 
     let pageIndex = 0;
     let time = 30000;
     const filter = (reaction, user) => {
         return reactions.includes(reaction.emoji.name) && user.id === message.author.id;
     };
+
     if (options) {
         if (options.time) time = options.time
-    }
+    };
+
+    if (pageMsg.deleted) return;
+
     const collector = pageMsg.createReactionCollector(filter, { time: time });
     collector.on('collect', (reaction, user) => {
         reaction.users.remove(user)
@@ -188,12 +196,15 @@ async function paginate(message, embeds, options) {
                 pageMsg.edit({ embed: embeds[pageIndex] })
             } else {
                 pageIndex = embeds.length-1
-                pageMsg.edit({ embed: embeds[pageIndex]})
+                pageMsg.edit({ embed: embeds[pageIndex] })
             }
         }
     });
 
-    collector.on('end', () => pageMsg.reactions.removeAll().catch(err => console.log(err)));
+    collector.on('end', () => {
+        if (pageMsg.deleted) return;
+        pageMsg.reactions.removeAll().catch(err => console.log(err));
+    });
 }
 
 /**
