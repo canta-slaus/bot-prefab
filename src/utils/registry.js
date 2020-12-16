e3f4347b9c0b48280e3eee326a670354451a433e
canta slaus <36232888+canta-slaus@users.noreply.github.com>
2020-12-17 00:46:16 +0100

    added a few checks

--- src/utils/registry.js
+++ src/utils/registry.js
@@ -2,48 +2,61 @@ const fs = require('fs').promises;
 const path = require('path');
 const { log } = require('./utils')
 
-async function registerCommands(client, dir) {
-    let files = await fs.readdir(path.join(__dirname, dir));
-    // Loop through each file.
-    for(let file of files) {
-        let stat = await fs.lstat(path.join(__dirname, dir, file));
-        if(stat.isDirectory()) // If file is a directory, recursive call recurDir
-            registerCommands(client, path.join(dir, file));
-        else {
-            // Check if file is a .js file.
-            if(file.endsWith(".js")) {
-                try {
-                    let cmdModule = require(path.join(__dirname, dir, file));
-                    let { name, aliases, category } = cmdModule;
+async function registerCommands(client, ...dirs) {
+    for (const dir of dirs) {
+        let files = await fs.readdir(path.join(__dirname, dir));
+        // Loop through each file.
+        for(let file of files) {
+            let stat = await fs.lstat(path.join(__dirname, dir, file));
+            if(stat.isDirectory()) // If file is a directory, recursive call recurDir
+                registerCommands(client, path.join(dir, file));
+            else {
+                // Check if file is a .js file.
+                if(file.endsWith(".js")) {
+                    try {
+                        let cmdModule = require(path.join(__dirname, dir, file));
+                        let { name, aliases, category, execute } = cmdModule;
 
-                    if (!name) {
-                        log("WARNING", "src/registry.js", `The command '${path.join(__dirname, dir, file)}' doesn't have a name`)
-                        continue;
+                        if (!name) {
+                            log("WARNING", "src/registry.js", `The command '${path.join(__dirname, dir, file)}' doesn't have a name`);
+                            continue;
+                        }
+
+                        if (!execute) {
+                            log("WARNING", "src/registry.js", `The command '${name}' doesn't have an execute function`);
+                            continue;
+                        }
+
+                        if (client.commands.has(name)) {
+                            log("WARNING", "src/registry.js", `The command name '${name}' has already been added.`);
+                            continue;
+                        }
+
+                        client.commands.set(name, cmdModule);
+
+                        if (category) {
+                            let commands = client.categories.get(category.toLowerCase());
+                            if (!commands) commands = [category];
+                            commands.push(name);
+                            client.categories.set(category.toLowerCase(), commands);
+                        } else {
+                            log("WARNING", "src/registry.js", `The command '${name}' doesn't have a category, it will default to 'No category'.`);
+                            let commands = client.categories.get('no category');
+                            if (!commands) commands = ['No category'];
+                            commands.push(name);
+                            client.categories.set('no category', commands);
+                        }
+
+                        if(aliases && aliases.length !== 0) {
+                            aliases.forEach(alias => {
+                                if (client.commands.has(alias)) {
+                                    log("WARNING", "src/registry.js", `The command alias '${alias}' has already been added.`);
+                                } else client.commands.set(alias, cmdModule);
+                            });
+                        }
+                    } catch (e) {
+                        log("ERROR", "src/registry.js", `Error loading commands: ${e.message}`);
                     }
-                    if (!cmdModule.execute) {
-                        log("WARNING", "src/registry.js", `The command '${name}' doesn't have an execute function`)
-                        continue;
-                    }
-
-                    client.commands.set(name, cmdModule);
-
-                    if (category) {
-                        let commands = client.categories.get(category.toLowerCase())
-                        if (!commands) commands = [category]
-                        commands.push(name)
-                        client.categories.set(category.toLowerCase(), commands)
-                    } else {
-                        log("WARNING", "src/registry.js", `The command '${name}' doesn't have a category, it will default to 'No category'.`)
-                        let commands = client.categories.get('no category')
-                        if (!commands) commands = ['No category']
-                        commands.push(name)
-                        client.categories.set('no category', commands)
-                    }
-
-                    if(aliases && aliases.length !== 0)
-                        aliases.forEach(alias => client.commands.set(alias, cmdModule));
-                } catch (e) {
-                    log("ERROR", "src/registry.js", `Error loading commands: ${e.message}`);
                 }
             }
         }
